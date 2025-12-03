f7932006fb9bb38e31da2b5c0a1f8aef149b0de9
scott2b <scott2b>
2025-12-03 15:20:18 -0600

    fix rendering

--- sync_root.sh
+++ sync_root.sh
@@ -3,18 +3,13 @@ set -e
 
 TEMP_DIR="$(mktemp -d)"
 
-# 1. Render from this repo into a temp directory.
-# copier.yml must have: _subdirectory: template
+# 1. Use template/ as the Copier template root
 copier copy \
     --defaults \
-    . \
+    template \
     "$TEMP_DIR"
 
-# 2. Remove the *rendered* template directory from the temp output,
-#    so we don't overwrite our canonical template/ in the repo.
-rm -rf "$TEMP_DIR/template"
-
-# 3. Remove everything at the root except the things we want to keep.
+# 2. Remove everything at the root except what we want to keep
 find . -mindepth 1 -maxdepth 1 \
     ! -name ".git" \
     ! -name ".gitignore" \
@@ -23,7 +18,7 @@ find . -mindepth 1 -maxdepth 1 \
     ! -name "DOCS.md" \
     -exec rm -rf {} +
 
-# 4. Copy rendered project files into the root.
+# 3. Copy rendered project files into the root
 cp -R "$TEMP_DIR"/. .
 
 echo "Root synchronized from Copier template."
